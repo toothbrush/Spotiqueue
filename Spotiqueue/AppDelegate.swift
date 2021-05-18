a5ff079ffbd5f114f1688463acfe2fe0c66d90df
paul <423357+toothbrush@users.noreply.github.com>
2021-05-18 15:33:57 +1000

    Add SpotifyAPI from Peter Schorn

--- Spotiqueue/AppDelegate.swift
+++ Spotiqueue/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var searchField: NSSearchFieldCell!
     @IBOutlet weak var window: NSWindow!
 
+    // Hooking up the Array Controller it was helpful to read     https://swiftrien.blogspot.com/2015/11/swift-example-binding-nstableview-to.html
+    // I also had to follow advice here https://stackoverflow.com/questions/46756535/xcode-cannot-resolve-the-entered-path-when-binding-control-in-xib-file because apparently in newer Swift, @objc dynamic isn't implied.
+    // Here is another extensive howto around table views and such https://www.raywenderlich.com/921-cocoa-bindings-on-macos
+
     @objc dynamic var searchResults: Array<SpotifySongTableRow> = []
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
