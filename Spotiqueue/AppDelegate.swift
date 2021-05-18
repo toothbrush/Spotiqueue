80bf817e3132c8d6cb528bd5cb327eb7a180a3c7
paul <423357+toothbrush@users.noreply.github.com>
2021-05-18 11:18:33 +1000

    Initial Commit

---
+++ Spotiqueue/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Spotiqueue
+//
+//  Created by Paul on 18/5/21.
+//  Copyright © 2021 Rustling Broccoli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
