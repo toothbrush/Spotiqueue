43f8e3926e094e88324baeabc5bcf3873c489e80
paul <423357+toothbrush@users.noreply.github.com>
2021-05-20 21:24:03 +1000

    TrackExtensions: Teach a Track how to display artists nicely.

---
+++ Spotiqueue/Extensions/TrackExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  TrackExtensions.swift
+//  Spotiqueue
+//
+//  Created by Paul on 20/5/21.
+//  Copyright © 2021 Rustling Broccoli. All rights reserved.
+//
+
+import Foundation
+import SpotifyWebAPI
+
+extension Track {
+    func consolidated_name() -> String {
+        guard self.artists != nil else {
+            return "<no artist>"
+        }
+
+        let names = self.artists!.map { (artist) -> String in
+            artist.name
+        }
+
+        let proposedName = names.prefix(3).joined(separator: ", ")
+
+        if artists!.count > 3 {
+            return proposedName + ", …"
+        } else {
+            return proposedName
+        }
+    }
+}
