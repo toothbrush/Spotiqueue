e2027ecd927210c5dfbfdb6cd6310e9d8213ef72
paul <423357+toothbrush@users.noreply.github.com>
2021-05-22 12:34:34 +1000

    lib.rs(bar): Create an example extern "C" function.

---
+++ spotiqueue-worker/spotiqueue_worker_bindings.h
@@ -0,0 +1,6 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t bar(uint32_t a);
--- spotiqueue-worker/src/lib.rs
+++ spotiqueue-worker/src/lib.rs
@@ -5,3 +5,11 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[allow(dead_code)]
+#[no_mangle]
+pub extern "C" fn bar(a: u32) -> u32 {
+    // this can be called from Objective-C
+    println!("somebody called bar()");
+    return a + 3;
+}
