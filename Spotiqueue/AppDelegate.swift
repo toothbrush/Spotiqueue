02145c002537d3f8c0be327adaa3bdc20d2db323
paul <423357+toothbrush@users.noreply.github.com>
2021-05-23 19:17:13 +1000

    AppDelegate.swift: Move declaration up top.

--- Spotiqueue/AppDelegate.swift
+++ Spotiqueue/AppDelegate.swift
@@ -26,6 +26,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var searchFieldCell: NSSearchFieldCell!
     @IBOutlet weak var window: NSWindow!
 
+    @objc dynamic var searchResults: Array<RBSpotifySongTableRow> = []
+
     var spotify: RBSpotify = RBSpotify()
 
     private var cancellables: Set<AnyCancellable> = []
@@ -33,8 +35,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // Hooking up the Array Controller it was helpful to read https://swiftrien.blogspot.com/2015/11/swift-example-binding-nstableview-to.html
     // I also had to follow advice here https://stackoverflow.com/questions/46756535/xcode-cannot-resolve-the-entered-path-when-binding-control-in-xib-file because apparently in newer Swift, @objc dynamic isn't implied.
     // Here is another extensive howto around table views and such https://www.raywenderlich.com/921-cocoa-bindings-on-macos
-
-    @objc dynamic var searchResults: Array<RBSpotifySongTableRow> = []
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         initialiseSpotifyLibrary()
