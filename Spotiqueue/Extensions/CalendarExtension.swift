514f481e7d1fea01963ba6fc8f5eaea7d782eb43
paul <423357+toothbrush@users.noreply.github.com>
2021-05-24 20:25:49 +1000

    Little Calendar extension.

---
+++ Spotiqueue/Extensions/CalendarExtension.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarExtension.swift
+//  Spotiqueue
+//
+//  Created by Paul on 24/5/21.
+//  Copyright © 2021 Rustling Broccoli. All rights reserved.
+//
+
+import Foundation
+
+extension Calendar {
+    static let iso8601 = Calendar(identifier: .iso8601)
+}
