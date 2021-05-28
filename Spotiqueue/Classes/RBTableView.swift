184ef10cbc08f38b89f42fd2e83f48b592485f26
paul <423357+toothbrush@users.noreply.github.com>
2021-05-28 22:21:08 +1000

    Cmd-; as album-browse instead.

--- Spotiqueue/Classes/RBTableView.swift
+++ Spotiqueue/Classes/RBTableView.swift
@@ -89,6 +89,9 @@ class RBTableView: NSTableView {
         } else if flags.isSuperset(of: .command)
                     && event.keyCode == 124 { // cmd-right, search for album
             searchForAlbum()
+        } else if flags.isSuperset(of: .command)
+                    && event.characters == ";" { // cmd-;, search for album, because cmd-L is taken?
+            searchForAlbum()
         } else if event.characters == "/"
                     && flags.isEmpty {
             focusSearchField()
