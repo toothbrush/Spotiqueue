110b5aea1490aa7f1d0bc56afd54348318db878a
paul <423357+toothbrush@users.noreply.github.com>
2021-06-12 15:35:06 +1000

    Copy header file, too.

--- Spotiqueue-Bridging-Header.h
+++ Spotiqueue-Bridging-Header.h
@@ -2,4 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include "spotiqueue-worker/spotiqueue_worker_bindings.h"
+#include "spotiqueue_worker/spotiqueue_worker_bindings.h"
