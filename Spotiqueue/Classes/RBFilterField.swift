e4cf52e498094a9f64f4582a8c298d5e632185b5
paul <423357+toothbrush@users.noreply.github.com>
2021-07-02 13:02:57 +1000

    Catch errors when building regex from filter string.

--- Spotiqueue/Classes/RBFilterField.swift
+++ Spotiqueue/Classes/RBFilterField.swift
@@ -23,4 +23,33 @@ class RBFilterField: NSTextField {
         }
     }
 
+    func buildFilter(filterString: String) -> NSPredicate? {
+        var filter: NSPredicate? = nil
+        do {
+            // We don't like exceptions, so let's attempt to build the regex before .. using it.  There's likely a more performant way of doing this.  It'd be great to have a version of NSPredicate that throws, but that doesn't appear to exist.
+            _ = try NSRegularExpression(pattern: filterString, options: [])
+
+            // The Predicate Programming Guide: https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Predicates/AdditionalChapters/Introduction.html#//apple_ref/doc/uid/TP40001789
+            // "[cd]" specifies case and diacritic insensitivity
+            filter = NSPredicate(format: "SELF.title MATCHES [cd] %@", argumentArray: [filterString])
+        }
+        catch {
+            logger.warning("Filter string '\(filterString)' isn't a valid regex.")
+        }
+
+        return filter
+    }
+
+    override func textDidChange(_ notification: Notification) {
+        // If the filter field is blanked, we want to display all items.
+        if self.stringValue == "" {
+            AppDelegate.appDelegate().searchResultsArrayController.filterPredicate = nil
+        } else {
+            // We don't directly build the predicate from the filter string because it's.. subtle.
+            if let predicate = buildFilter(filterString: self.stringValue) {
+                AppDelegate.appDelegate().searchResultsArrayController.filterPredicate = predicate
+            }
+        }
+        super.textDidChange(notification)
+    }
 }
