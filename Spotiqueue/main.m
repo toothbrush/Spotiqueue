e89d3172a1294c7e6c33317cdd0fc8ade162bb12
paul <423357+toothbrush@users.noreply.github.com>
2021-09-06 20:01:39 +1000

    Whoa, it works!

--- Spotiqueue/main.m
+++ Spotiqueue/main.m
@@ -6,10 +6,27 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <libguile.h>
+
+static void* register_functions (void* data)
+{
+    SCM test = scm_c_eval_string("(+ 3 5)");
+    int foo = scm_to_int(test);
+    printf("int foo = %d\n", foo);
+
+    scm_c_eval_string("(begin (display 'HelloWorld) (newline))");
+    return NULL;
+}
+
+void run_guile(void) {
+    printf("hello from C, before Guile\n");
+    scm_init_guile();
+    scm_with_guile(&register_functions, NULL);
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        run_guile();
         NSLog(@"Whee how weird");
     }
     return NSApplicationMain(argc, argv);
