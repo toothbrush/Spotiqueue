81643d5ce6c7c13001e46ca134459ed5be8b7cc7
paul <423357+toothbrush@users.noreply.github.com>
2021-09-09 21:51:02 +1000

    Keep track of what i've played, ever.

--- examples/paul-config.scm
+++ examples/paul-config.scm
@@ -1,10 +1,22 @@
 ;;; This is an example of what would live in a user's config.
+(use-modules (ice-9 textual-ports))
+
+(define* (write-text path text #:key (append #f))
+  (let ((file (open-file path (if append "a" "w"))))
+    (put-string file text)
+    (close-port file)))
+
+(define my-homedir (spotiqueue:get-homedir))
+
 (define (paul:player-started song)
   (if (not (song? song))
       (error "eek, not a song!"))
   (begin
-    (format #t "hey, a song has started: ~s" song)
-    (newline)))
+    (format #t "hey, a song has started: ~s\n" song)
+    (write-text
+     (string-append my-homedir "/spotiqueue-played.txt")
+     (format #f "~a\n" song)
+     #:append #t)))
 
 (define (paul:player-endoftrack song)
   (if (not (song? song))
