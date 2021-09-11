fa5950adf4fdccadf022329e132342876380fc97
paul <423357+toothbrush@users.noreply.github.com>
2021-09-11 14:28:14 +1000

    keybindings.scm: New empty Guile file.

---
+++ Spotiqueue/guile/keybindings.scm
@@ -0,0 +1,9 @@
+;;; BEGIN keybindings.scm
+;;;
+;;; Copyright © 2021 paul at denknerd dot org
+;;;
+;;; This file contains code used to define custom keymaps and bindings.
+
+
+
+;;; END keybindings.scm
