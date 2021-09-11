012a4ccfde0e98d3fdb17497bdf4aba1b823fd2c
paul <423357+toothbrush@users.noreply.github.com>
2021-09-11 17:05:15 +1000

    Get keysyms working!

--- Spotiqueue/guile/keybindings.scm
+++ Spotiqueue/guile/keybindings.scm
@@ -23,13 +23,12 @@
    "pwpwpwpwpw" ; p = protected/boxed/SCM value, w = writable (deprecated/ignored)
    (lambda (kbd port)
      ;; I keep getting warnings, probably because (ice-9 format) isn't yet loaded at compile time... ^.-
-     (let ((keysym (hash-ref keycode->keysym (kbd-key kbd))))
-       (format port "<<key> ~@[C-~]~@[H-~]~@[M-~]~@[S-~]~a>"
-               (kbd-ctrl kbd)
-               (kbd-cmd kbd)
-               (kbd-alt kbd)
-               (kbd-shift kbd)
-               keysym)))))
+     (format port "<<key> ~@[C-~]~@[H-~]~@[M-~]~@[S-~]~a>"
+             (kbd-ctrl kbd)
+             (kbd-cmd kbd)
+             (kbd-alt kbd)
+             (kbd-shift kbd)
+             (hash-ref keycode->keysym (kbd-key kbd))))))
 
 (define (kbd-ctrl kbd)
   (struct-ref kbd 0))
@@ -42,17 +41,21 @@
 (define (kbd-key kbd)
   (struct-ref kbd 4))
 
-(define C-f (make-struct/no-tail <kbd> #t #f #f #f 'ANSI_F)) ; this would represent C-f in as-low-level
-                                        ; as i can muster, for now.
+(define* (kbd keysym #:key (ctrl #f) (cmd #f) (alt #f) (shift #f))
+  ;; Why doesn't hash-ref work here?!  Is it because symbols somehow "belong" to the module where
+  ;; they've been defined?  Anyway, hashq-ref solves the issue for me, for now.
+  (let ((keycode (hashq-ref keysym->keycode keysym)))
+    (display keycode)
+    (newline)
+    (if keycode
+        (make-struct/no-tail <kbd> ctrl cmd alt shift keycode)
+        (raise-exception "Unknown key specified."))))
 
-(define* (kbd keysym #:key
-              (ctrl #f)
-              (cmd #f)
-              (alt #f)
-              (shift #f))
-  (make-struct/no-tail <kbd> ctrl cmd alt shift keysym))
+;; An example of how i would represent C-f in as-low-level a fashion as i can muster, for now.
+;; Don't do this though, it's painful.  Mostly useful for constructing values in Swift-land.
+(define example_C-f (make-struct/no-tail <kbd> #t #f #f #f 3))
 
+;; An example of Alt-X
 (define M-x (kbd 'ANSI_X #:alt #t))
-
 
 ;;; END keybindings.scm
