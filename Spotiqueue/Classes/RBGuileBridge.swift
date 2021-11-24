ab81550550944f3cb658d36151ec5cbb8566fb04
paul <423357+toothbrush@users.noreply.github.com>
2021-11-24 20:29:23 +1100

    RBGuileBridge.swift(guile_handle_key): Clarify "key not bound" output.

--- Spotiqueue/Classes/RBGuileBridge.swift
+++ Spotiqueue/Classes/RBGuileBridge.swift
@@ -176,8 +176,10 @@ public func track_to_scm_record(track: RBSpotifyItem) -> SCM {
 
         if !_scm_is_true(action) {
             scm_simple_format(_scm_true(),
-                              scm_from_utf8_string("~a not bound by user.~%"),
-                              scm_list_1(guile_key))
+                              scm_from_utf8_string("[keymap=~a] ~a not bound by user.~%"),
+                              scm_list_2(
+                                scm_from_utf8_string(map.rawValue.cString(using: .utf8)),
+                                guile_key))
             return false
         }
 
